eaa350e0f1de23fabf8d97bc2793286d4ae609af
abreens <axel@abrconsultancy.be>
2019-09-25 22:08:07 +0200

    Les8 updates

---
+++ les8/les8_intro.py
@@ -0,0 +1,25 @@
+# Een simpele print
+print("Hello World")
+
+# Some variables
+x = 5
+y = 2
+print(x + y)
+print(x, y, x + y) # Inline comments
+
+"""
+This is a block comment
+"""
+
+# Een string
+some_string = "Hey there!"
+print(some_string)
+
+# Input
+print('Enter your name:')
+x = input()
+print('Hello, ' + x)
+
+user_name = input("Please enter your name: ")
+print("Hello " + user_name + "!")
+
