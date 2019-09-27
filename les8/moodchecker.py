b9d2f1f70e9926586fe07a8c376e83c3512e2dc4
abreens <axel@abrconsultancy.be>
2019-09-27 21:34:39 +0200

    moodchecker upload

---
+++ les8/moodchecker.py
@@ -0,0 +1,19 @@
+mood = input("What is your mood today ('happy' or 'nervous' or 'sad' or 'excited')? ")
+
+if mood == "happy":
+    print("It is great to see you happy. Celebrate with a DUVEL!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times... and then I suggest to drink a DUVEL!")
+
+elif mood == "sad":
+    print("Cheer up, mate... drink a DUVEL!")
+
+elif mood == "excited":
+    print("Woohoo. Time for a DUVEL!")
+
+elif mood == "relaxed":
+    print("Yeah, today is the day to chill... and drink a DUVEL!")
+
+else:
+    print("I don't recognize this mood. I'll suggest you drink a DUVEL and try again...")
