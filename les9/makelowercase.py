8af32fea4025449e69048a652b80b83b56677973
abreens <axel@abrconsultancy.be>
2019-09-28 11:12:24 +0200

    makelowercase upload

---
+++ les9/makelowercase.py
@@ -0,0 +1,21 @@
+#####
+# Les 9 - Make string lowercase
+#
+# Sometimes you'd like to make some string lowercase.
+# For example, you have a string like this: "Today Is A BeautiFul DAY"
+# And you'd like to make it like this: "today is a beautiful day"
+#
+# There is a very nice solution in Python to do this. Use Google search and find out how to do it.
+#####
+
+# Welcome
+print("Welcome to the Smartninja String Converting Service!\n")
+
+# Input en conversie
+s = input("Geef een string aub: ")
+print("\nConverting...")
+print(s.lower())
+
+# Afscheid
+print ("\nBedankt voor het gebruiken van onze service!")
+
