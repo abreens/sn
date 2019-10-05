865c2c954c444579303920ceaa1c398c48e5755b
abreens <axel@abrconsultancy.be>
2019-10-05 17:00:46 +0200

    Upload homework les12

---
+++ les12/modularsecret.py
@@ -0,0 +1,146 @@
+#####
+# Les 12 - Make the "Guess the secret number" game more modular
+#
+# Use functions to make the game more modular. Also try to add another while loop so the user can play
+# many rounds of the game without having to re-run the program each time.
+# You'll probably need three functions:
+#    play_game()
+#    get_score_list()
+#    get_top_scores()
+# You can let the user choose the level of the game: 'Easy' of 'Hard'
+# The easy level has suggestions "try something smaller" or "try something bigger".
+# The hard level does not have these suggestions, it only tells you if your guess is right or wrong.
+#
+#####
+
+# Nodige external packages importeren
+import random, json, datetime
+
+# FUNKTIE - Inlezen van een geheel getal tussen 1 en <upper_limit>
+def lees_geheel(upper_limit):
+    while True:
+        invoer = input("\nGeef een getal (tussen 1 en " + str(upper_limit) + "): ")
+        try:
+            getal = int(invoer)
+        except ValueError:
+            print("Jammer genoeg was dit geen (geheel) getal. Probeer aub opnieuw...")
+        else:
+            if getal >= 1 and getal <= upper_limit:
+                # De lus breken
+                break
+            else:
+                print("Het getal moet tussen 1 en " + str(upper_limit) + " liggen. Probeer aub opnieuw...")
+    return (getal)
+
+# FUNKTIE - Inlezen van de naam van de speler
+def lees_naam():
+    while True:
+        name = input("Wat is Uw naam? ")
+        # Empty strings return False!
+        if not name:
+            print("Naam mag niet leeg zijn. Gelieve opnieuw Uw naam in te voeren.\n")
+        else:
+            # De lus breken
+            break
+    return(name)
+
+# FUNKTIE - Inlezen van een letter van lijst [mogelijkheden]
+# Zowel kleine als hoofdletter zijn toegestaan. msg is de boodschap voor de gebruiker
+def lees_letter(msg, mogelijkheden):
+    while True:
+        invoer = input(msg)
+        invoer = invoer.upper()
+        if invoer in mogelijkheden:
+            return(invoer)
+            break
+        else:
+            print("Dat was geen correct invoer. probeert U het aub opnieuw...\n")
+
+# FUNKTIE - Toon de huidige top 3 scores
+def get_top_scores():
+    with open("score_list.txt", "r") as score_file:
+        score_list = json.loads(score_file.read())
+        # Empty lists return False
+        if not score_list:
+            print("\nEr zijn nog geen top scores opgeladen!\n")
+        else:
+            print("\nDe huidige top scores zijn:")
+
+            # Sort the list of dictionaries per attempts and keep only the top 3 best scores
+            new_score_list = sorted(score_list, key=lambda k: k['attempts'])[:3]
+
+            # Print the new sorted scores
+            for score_dict in new_score_list:
+                print("Speler {} had op {} {} poging(en) nodig om het geheim getal {} te raden. Wrong guesses: {}"
+                    .format(score_dict.get("speler"), score_dict.get("date"), str(score_dict.get("attempts")),
+                    str(score_dict.get("secret")), str(score_dict.get("wrong_guesses"))))
+            print()
+
+# FUNKTIE - Speel het spel
+def play_game(name, level):
+
+    # Initialisaties
+    secret = random.randint(1, 30)
+    attempts = 0
+    dts = str(datetime.datetime.now())  # dts = Date Time Stamp
+    wrong_guesses = []
+
+    while True:
+        guess = lees_geheel(30)
+        attempts += 1
+
+        # Het antwoord analyseren
+        if guess == secret:
+            print("\nYou've guessed it - congratulations! It's number " + str(secret))
+            print("Attempts needed: " + str(attempts))
+            print("Dit waren Uw verkeerde keuzes: ", wrong_guesses)
+            print()
+
+            # De score_list.txt file updaten en wegschrijven
+            with open("score_list.txt", "r") as score_file:
+                score_list = json.loads(score_file.read())
+                score_list.append({"attempts": attempts, "date": dts, "speler": name, "secret": secret,
+                                   "wrong_guesses": wrong_guesses})
+            with open("score_list.txt", "w") as score_file:
+                score_file.write(json.dumps(score_list))
+
+            # De lus breken
+            break
+
+        else:
+            # Het getal werd niet geraden.
+            print("Sorry, Uw gok is niet correct...")
+            if level == "easy":
+                # Een tip voor de gebruiker meegeven als mode 'Easy' is
+                if guess > secret:
+                    print("Tip: try something smaller")
+                else:
+                    print("Tip: try something bigger")
+        # De huidige guess dynamisch opslaan in lijst wrong_guesses
+        wrong_guesses.append(guess)
+
+# 1. Welcome
+print("\nDit is het 'Guess the Secret Number' spel!\n")
+
+# 2. Naam van de speler inlezen
+naam = lees_naam()
+print("Welkom, " + naam + "!\n")
+
+# 3. Main game loop
+while True:
+    boodschap = "Would you like to A) play a new game, B) see the best scores, or C) quit? "
+    selection = lees_letter(boodschap, ["A", "B", "C"])
+    if selection == "A":
+        boodschap = "Wil U het (E)asy of (H)ard level? "
+        level = lees_letter(boodschap, ["E", "H"])
+        if level == "E":
+            play_game(naam, "easy")
+        else:
+            play_game(naam, "hard")
+    elif selection == "B":
+        get_top_scores()
+    else:
+        break
+
+# 4. Afscheid
+print("\nBedankt voor het spelen. Hope to see you soon...")
