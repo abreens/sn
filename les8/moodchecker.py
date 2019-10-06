dde191af9184a1b1dbb0fc8244083bc11773446a
abreens <axel@abrconsultancy.be>
2019-10-06 12:09:56 +0200

    Corrected code conventions

--- les8/moodchecker.py
+++ les8/moodchecker.py
@@ -1,19 +1,19 @@
 mood = input("What is your mood today ('happy' or 'nervous' or 'sad' or 'excited' or 'relaxed')? ")
 
 if mood == "happy":
-    print("It is great to see you happy. Celebrate with a DUVEL!")
+    print('It is great to see you happy. Celebrate with a DUVEL!')
 
 elif mood == "nervous":
-    print("Take a deep breath 3 times... and then I suggest to drink a DUVEL!")
+    print('Take a deep breath 3 times... and then I suggest to drink a DUVEL!')
 
 elif mood == "sad":
-    print("Cheer up, mate... drink a DUVEL!")
+    print('Cheer up, mate... drink a DUVEL!')
 
 elif mood == "excited":
-    print("Woohoo. Time for a DUVEL!")
+    print('Woohoo. Time for a DUVEL!')
 
 elif mood == "relaxed":
-    print("Yeah, today is the day to chill... and drink a DUVEL!")
+    print('Yeah, today is the day to chill... and drink a DUVEL!')
 
 else:
     print("I don't recognize this mood. I'll suggest you drink a DUVEL and try again...")
