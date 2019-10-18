dfd657fb7fb2d3e4508c85c91f5f1c41220438e8
abreens <axel@abrconsultancy.be>
2019-10-18 20:57:05 +0200

    code ready

---
+++ les15/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # Connect handler to index.html
+    return render_template("index.html")
+
+@app.route("/about")
+def about_me():
+    # Connect handler to about_me.html
+    return render_template("about_me.html")
+
+@app.route("/portfolio")
+def portfolio():
+    # Connect handler to portfolio.html
+    return render_template("portfolio.html")
+
+
+if __name__ == '__main__':
+    app.run()
