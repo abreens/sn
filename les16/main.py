f29bc0f7b52d693149c3a60f5ac9ec707614e24e
Axel Breens <axel@abrconsultancy.be>
2019-10-27 12:08:41 +0100

    Cont'd Development

--- les16/main.py
+++ les16/main.py
@@ -18,8 +18,10 @@ def index():
     some_text = "Message from the handler."
     current_year = datetime.datetime.now().year
 
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
     # Connect handler to /index.html
-    return render_template("index.html", some_text=some_text, current_year=current_year)
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
 
 @app.route("/about")
