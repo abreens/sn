de20e913297322db9d396f2733e73264168b5d52
Axel Breens <axel@abrconsultancy.be>
2019-11-13 06:30:54 +0100

    Continued Development

---
+++ les202/main.py
@@ -0,0 +1,23 @@
+###
+#
+# Homework 20.2 - GitHub login
+#
+###
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+   return render_template("profile.html")
+
+
+if __name__ == '__main__':
+    app.run()
