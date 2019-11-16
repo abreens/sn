d266c58760a7c076f33e52b58a9c12f0bc071a32
Axel Breens <axel@abrconsultancy.be>
2019-11-16 17:55:47 +0100

    Continued Development

---
+++ les210/models.py
@@ -0,0 +1,15 @@
+import os
+from sqla_wrapper import SQLAlchemy
+
+# this connects to a database either on Heroku or on localhost
+db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)  # a username must be unique! Two users cannot have the same username
+    email = db.Column(db.String, unique=True)  # email must be unique! Two users cannot have the same email address
+    secret_number = db.Column(db.Integer, unique=False)  # must NOT be unique across user object
+    password = db.Column(db.String)
+    session_token = db.Column(db.String)
+
