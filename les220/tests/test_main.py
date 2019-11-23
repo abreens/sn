1b095a2fca3d1df5300adeb05ab82f615be03f5d
Axel Breens <axel@abrconsultancy.be>
2019-11-23 16:44:28 +0100

    Continued Development

--- les220/tests/test_main.py
+++ les220/tests/test_main.py
@@ -1,6 +1,9 @@
 import os
 import pytest
 from main import app, db
+from models import User
+
+
 
 
 @pytest.fixture
@@ -27,6 +30,80 @@ def test_index_logged_in(client):
                                 "user-password": "123"}, follow_redirects=True)
     response = client.get('/index')
     assert b'Uw gok?' in response.data
+
+
+# def test_index_wrong_password(client):
+#    client.post('/login', data={"user-name": "TestUser", "user-email": "testuser@telenet.be",
+#                                "user-password": "wrongpassword"}, follow_redirects=True)
+#    response = client.get('/login')
+#    assert b'WRONG PASSWORD!' in response.data
+
+def test_result_correct(client):
+    # create a user
+    client.post('/login', data={"user-name": "TestUser", "user-email": "testuser@telenet.be",
+                                "user-password": "123"}, follow_redirects=True)
+
+    # get the first (and only) user object from the database
+    user = db.query(User).first()
+
+    # set the secret number to 22, so that you can make a success "guess" in the test.
+    user.secret_number = 22
+    db.add(user)
+    db.commit()
+
+    response = client.post('/result', data={"guess": 22})  # enter the correct guess
+    assert b'Het geheime nummer is inderdaad 22' in response.data
+
+
+def test_result_te_klein(client):
+    # create a user
+    client.post('/login', data={"user-name": "TestUser", "user-email": "testuser@telenet.be",
+                                "user-password": "123"}, follow_redirects=True)
+
+    # get the first (and only) user object from the database
+    user = db.query(User).first()
+
+    # set the secret number to 22, so that you can make a success "guess" in the test.
+    user.secret_number = 22
+    db.add(user)
+    db.commit()
+
+    response = client.post('/result', data={"guess": 20})  # guess is too small
+    assert b'try something bigger.' in response.data
+
+
+def test_result_te_groot(client):
+    # create a user
+    client.post('/login', data={"user-name": "TestUser", "user-email": "testuser@telenet.be",
+                                "user-password": "123"}, follow_redirects=True)
+
+    # get the first (and only) user object from the database
+    user = db.query(User).first()
+
+    # set the secret number to 22, so that you can make a success "guess" in the test.
+    user.secret_number = 22
+    db.add(user)
+    db.commit()
+
+    response = client.post('/result', data={"guess": 24})  # guess is too big
+    assert b'try something smaller.' in response.data
+
+
+def test_result_out_of_bound_low(client):
+    # create a user
+    client.post('/login', data={"user-name": "TestUser", "user-email": "testuser@telenet.be",
+                                "user-password": "123"}, follow_redirects=True)
+
+    # get the first (and only) user object from the database
+    user = db.query(User).first()
+
+    # set the secret number to 22, so that you can make a success "guess" in the test.
+    user.secret_number = 22
+    db.add(user)
+    db.commit()
+
+    response = client.post('/result', data={"guess": 0})  # guess is out of bound
+    assert b'Het getal moet tussen 1 en 30 liggen.' in response.data
 
 
 def cleanup():
