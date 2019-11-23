a4b79a0e3308879f64e80dedbea6d30315c8ebac
Axel Breens <axel@abrconsultancy.be>
2019-11-23 15:59:42 +0100

    Continued Development

---
+++ les220/tests/test_main.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+from main import app, db
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    os.environ["DATABASE_URL"] = "sqlite:///:memory:"
+    client = app.test_client()
+
+    cleanup()  # clean up before every test
+
+    db.create_all()
+
+    yield client
+
+
+def test_index_not_logged_in(client):
+    response = client.get('/index')
+    # This is the actual test
+    assert b'Enter your nam' in response.data
+
+
+def cleanup():
+    # clean up/delete the DB (drop all tables in the database)
+    db.drop_all()
