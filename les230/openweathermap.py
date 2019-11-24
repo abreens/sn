b756189292d4536f869a765dae2bd98fe4240125
Axel Breens <axel@abrconsultancy.be>
2019-11-24 09:39:19 +0100

    Continued Development

---
+++ les230/openweathermap.py
@@ -0,0 +1,8 @@
+# this file MUST be in .gitignore
+# do not upload it to GitHub!
+# if you upload it to GitHub, someone can steal your GitHub ID and secret!
+import os
+
+os.environ["GITHUB_CLIENT_ID"] = "06e7cf2ae00dad6a9565"
+os.environ["GITHUB_CLIENT_SECRET"] = "813fd752b3d0655fb3818568365d5be09011d6e9"
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
