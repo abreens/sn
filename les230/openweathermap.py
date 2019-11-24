5716935ca0eca8543381de3973cd8b66ab8dc57d
Axel Breens <axel@abrconsultancy.be>
2019-11-24 13:28:34 +0100

    Code ready

--- les230/openweathermap.py
+++
@@ -1,6 +0,0 @@
-# this file MUST be in .gitignore
-# do not upload it to GitHub!
-# if you upload it to GitHub, someone can steal your OWM API key!
-import os
-
-os.environ["OWM_API_KEY"] = "ab5cca117d2c140108b28d4dddb858d8"
