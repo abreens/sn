1a6226f7aff0d90be688d3259084dc79e2b9eb03
Axel Breens <axel@abrconsultancy.be>
2019-11-24 09:33:29 +0100

    Continued Development

--- les230/main.py
+++ les230/main.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    query = "London,UK"
+    unit = "metric"  # use "imperial" for Fahrenheit
+    api_key = "ab5cca117d2c140108b28d4dddb858d8"
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid={2}".format(query, unit, api_key)
+    data = requests.get(url=url)  # GET request to the OpenWeatherMap API
+
+    return render_template("index.html", data=data.json())
 
 
 if __name__ == '__main__':
